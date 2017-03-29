2b34f2a0137a93f6e9d4f0f9461055a6e10ea628
ProjectEight <s.frost@frostnet.co.uk>
2017-03-29 17:19:04 +0100

    Added module skeleton for ProjectEight_Ftp

---
+++ app/code/ProjectEight/Ftp/registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'ProjectEight_Ftp',
+        __DIR__
+    );
